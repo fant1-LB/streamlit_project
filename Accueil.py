a88649c9cddc97e7dc86bfd28d9a51f2a4671dfe
fant1-LB <fantinpro84@gmail.com>
2025-09-22 16:20:34 +0200

    appli sur pied

---
+++ Accueil.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.html("Bienvenue sur l'app de démo du travail du projet ANR <a href ='https://highvision.hypotheses.org/'> Highvision </a>. <br> Vous trouverez ci contre des exemples d'images du projet, de journaux du projet, et les liens qui ont pu être réalisés entre ceux-ci. ")
+st.write("Voici des exemples de circulations repérées manuellement dans le cadre du projet Highvision")
+df=pd.read_csv("liens_et_images.csv", sep=";")
+st.data_editor(df,
+    column_config={
+    "Image recto":st.column_config.ImageColumn("Image recto")},hide_index=True)
