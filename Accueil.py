3d91e6bd7f0bbb0255d5253204ca0e554c15bc48
fant1-LB <fantinpro84@gmail.com>
2025-09-22 17:05:00 +0200

    passé de dataeditor a dataframe

--- Accueil.py
+++ Accueil.py
@@ -4,6 +4,6 @@ import pandas as pd
 st.html("Bienvenue sur l'app de démo du travail du projet ANR <a href ='https://highvision.hypotheses.org/'> Highvision </a>. <br> Vous trouverez ci contre des exemples d'images du projet, de journaux du projet, et les liens qui ont pu être réalisés entre ceux-ci. ")
 st.write("Voici des exemples de circulations repérées manuellement dans le cadre du projet Highvision")
 df=pd.read_csv("liensghub_et_images.csv", sep=";")
-st.data_editor(df,
+st.dataframe(df,
     column_config={
     "Image recto":st.column_config.ImageColumn("Image recto", width="large"), "Image verso":st.column_config.ImageColumn("Image verso", width="large")},hide_index=True)
