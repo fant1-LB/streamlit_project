536574544774123836d115099c5267a28a6f1e68
fant1-LB <fantinpro84@gmail.com>
2025-09-22 16:45:37 +0200

    images completes et s'affichent

--- Accueil.py
+++ Accueil.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.html("Bienvenue sur l'app de démo du travail du projet ANR <a href ='https://highvision.hypotheses.org/'> Highvision </a>. <br> Vous trouverez ci contre des exemples d'images du projet, de journaux du projet, et les liens qui ont pu être réalisés entre ceux-ci. ")
 st.write("Voici des exemples de circulations repérées manuellement dans le cadre du projet Highvision")
-df=pd.read_csv("liens_et_images.csv", sep=";")
+df=pd.read_csv("liensghub_et_images.csv", sep=";")
 st.data_editor(df,
     column_config={
-    "Image recto":st.column_config.ImageColumn("Image recto")},hide_index=True)
+    "Image recto":st.column_config.ImageColumn("Image recto", width="large"), "Image verso":st.column_config.ImageColumn("Image verso", width="large")},hide_index=True)
